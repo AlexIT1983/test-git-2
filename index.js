8d20effc438de64c97b3d32874f03931a066a2ed
Алекс Антипов <alex@MacBook-Air-Aleks.local>
2024-01-15 15:45:40 +0300

    made changes in feature/user-access

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("Hello World");
 // console.error("Error");
-
+const age = 20;
+if (age >= 18) {
+  console.log("Вы совершенолетний");
+} else {
+  console.log("Вы не совершенолетний!");
+}
 // console.log("Testing branch")
