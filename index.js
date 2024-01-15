0d6c4831ba5715d949067c2f9302b8676f3c4673
Алекс Антипов <alex@MacBook-Air-Aleks.local>
2024-01-15 15:56:35 +0300

    made changes in master

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("Hello World");
 // console.error("Error");
-
+const userAge = 35;
+if (userAge >= 18) {
+  console.log("Вы совершенолетний");
+} else {
+  console.log("Вы не совершенолетний");
+}
 // console.log("Testing branch")
